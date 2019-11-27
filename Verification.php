353d50a9366a1236f7d2c75d9e19b4fcff9c8a66
Mamadou Ben TRAORE <tmadben@gmail.com>
2019-11-27 12:44:02 +0000

    update warning and table

--- Verification.php
+++ Verification.php
@@ -7,11 +7,27 @@
     <body>
         <?php 
             if (isset($_POST['login']) && (isset($_POST['passe']))) {
-
-            } else {
-                echo ('Les champs sont vides');
-            }
-        ?>
+                $login = $_POST['login'];
+                $passe = $_POST['passe'];
+                ?>
+                <table border="1">
+                    <tr>
+                        <td colspan=2> Tentative d'acces </td>
+                    </tr>
+                    <tr>
+                        <td>Login: </td>                        
+                        <td><?php echo ('$login') ?></td>
+                    </tr>
+                    <tr>
+                        <td>Mot de passe </td>
+                        <td><?php echo ('$passe') ?></td>
+                    </tr>
+                </table>
+            <?php    
+                } else {
+                    echo ('Les champs sont vides');
+                }
+            ?>
     </body>
     
 </html>
